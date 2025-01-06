1919962e1a3be3454a488df17e0178afc37a89e2
Arseni Ivanov <arseni_mars97@hotmail.com>
2025-01-06 11:56:34 +0100

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/arseniivanov.github.io/', // This is needed for GitHub Pages
   build: {
     rollupOptions: {
       input: {
         index: 'index.html',
-        cv: 'cv.html'
+        cv: 'cv.html',
+        blog: 'blog.html'  // Added blog.html since you have that page now
       }
     }
   }
